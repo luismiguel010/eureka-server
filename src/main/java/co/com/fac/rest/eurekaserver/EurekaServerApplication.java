e2afe87b129cae0e0e7c0209ca1da18b6e520122
Luis  Montes Zuluaga <luismonteszuluaga@MacBook-Air-de-Luis.local>
2020-02-07 09:11:25 -0500

    Add admin server configuration

--- src/main/java/co/com/fac/rest/eurekaserver/EurekaServerApplication.java
+++ src/main/java/co/com/fac/rest/eurekaserver/EurekaServerApplication.java
@@ -1,11 +1,15 @@
 package co.com.fac.rest.eurekaserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableAdminServer
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
